fecf0664308a7b56dd2f9528a736cd2733f7962d
muzahidul-maruf <marufmuzahidul@gmail.com>
2023-10-30 00:50:59 +0600

    first commit

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,41 @@
+<?php
+  
+namespace App\Jobs;
+
+use App\Mail\BulkEmailSend;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\SendEmailTest;
+use Mail;
+  
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+  
+    protected $details;
+  
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+  
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $email = new BulkEmailSend();
+        Mail::to($this->details['email'])->send($email);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('email-test', function () {
+
+    // $details['email'] = 'your_email@gmail.com';
+
+    $email = User::pluck('email');
+
+    $details['email'] = $email;
+
+    dispatch(new App\Jobs\SendEmailJob($details));
+
+    dd('done');
+});
